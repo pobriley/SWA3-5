d290a57d40213c7ccac4e10f78fe98320f88ab99
pobriley <59428856+pobriley@users.noreply.github.com>
2021-07-19 16:30:49 +0700

    Add files via upload

---
+++ SWA3-5/src/com/journaldev/composition/Person.java
@@ -0,0 +1,13 @@
+package com.journaldev.composition;
+
+public class Person {
+	private Job job;
+	   
+    public Person(){
+        this.job=new Job();
+        job.setSalary(1000L);
+    }
+    public long getSalary() {
+        return job.getSalary();
+    }
+}
